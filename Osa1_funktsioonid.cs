adf3a89c7c7ccf1be34bd67e9bb08c59dbe1f179
opilane <opilane@HP-CZC2349HT9>
2025-09-04 13:15:14 +0300

    Add project files.

---
+++ Osa1_funktsioonid.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_TARpv24
+{
+    internal class Osa1_funktsioonid
+    {
+        public static float Kalkulaator(float arv1, float arv2)
+        {
+            float k = arv1 * arv2;
+            return k;
+
+        }
+    }
+}
---
+++ Startclass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kordamine_TARpv24
+{
+    internal class Startclass
+    {
+        public static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Green;
+            
+            Console.WriteLine("Tere Tulemast!, Mis on sinu nimi?");
+            string tekst = Console.ReadLine();
+            Console.WriteLine($"{tekst}, Rõõm näha!");
+            int a = 10;
+            char taht = 'A';
+            Console.Write($"Esimene arv on {a}, sisesta b = ....");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Vastus on = {a + b}");
+            Console.WriteLine("Ujukomaarv");
+            double d = double.Parse(Console.ReadLine());
+            Console.WriteLine(d);
+            float f = float.Parse(Console.ReadLine());
+            Console.WriteLine(f);
+            bool g = false;
+
+            Random rnd = new Random();
+            a = rnd.Next(-100, 200);
+            Console.WriteLine(a);
+
+            float vastus = Osa1_funktsioonid.Kalkulaator(f, a);
+            Console.WriteLine($"Kalkulaatori tulemus: {vastus}");
+
+        }
+    }
+}
