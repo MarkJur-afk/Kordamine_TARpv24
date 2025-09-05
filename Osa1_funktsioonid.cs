5fe9dc01b2ff69b6fd22ed561b0991bc6a3c3182
opilane <opilane@HP-CZC2349HT9>
2025-09-05 11:39:55 +0300

    teine tund

--- Osa1_funktsioonid.cs
+++ Osa1_funktsioonid.cs
@@ -14,5 +14,104 @@ namespace Kordamine_TARpv24
             return k;
 
         }
+        public static string Kuu_nimetus(int kuu_nr)
+        {
+            string kuu = "";
+            switch (kuu_nr)
+            {
+                case 1: kuu = "Jaanuar"; break;
+                case 2: kuu = "Februar"; break;
+                case 3: kuu = "Märts"; break;
+                case 4: kuu = "April"; break;
+                case 5: kuu = "Mai"; break;
+                case 6: kuu = "Juni"; break;
+                case 7: kuu = "Juuli"; break;
+                case 8: kuu = "August"; break;
+                case 9: kuu = "September"; break;
+                case 10: kuu = "Oktoober"; break;
+                case 11: kuu = "November"; break;
+                case 12: kuu = "Detsember"; break;
+
+                default:
+                    kuu = "???";
+                    break;
+            }
+            return kuu;
+        }
+        public static string Hooaeg(int kuu_nr)
+        {
+            string hoo = "";
+            if (kuu_nr == 1 || kuu_nr == 2 || kuu_nr == 12) //&& - and, || - or
+            {
+                hoo = "Talv";
+            }
+            else if (kuu_nr > 2 && kuu_nr < 6)
+            {
+                hoo = "Kevad";
+            }
+            else if (kuu_nr > 5 && kuu_nr < 9)
+            {
+                hoo = "Suvi";
+            }
+            else if (kuu_nr > 8 && kuu_nr < 12)
+            {
+                hoo = "Sügis";
+            }
+            else
+            {
+                hoo = "???";
+            }
+
+            return hoo;
+        }
+        public static string Kino(int vanus)
+        
+        {
+            string hinn = "";
+            if (vanus > 0 && vanus < 7)
+            {
+                hinn = "Tasuta";
+            }
+            else if (vanus > 5 && vanus < 15)
+            {
+                hinn = "Lapse pilet";
+            }
+            else if (vanus > 14 && vanus < 66)
+            {
+                hinn = "Täis pilet";
+            }
+            else if (vanus > 65 && vanus < 100)
+            {
+                hinn = "Pensionäär";
+            }
+            else if (vanus < 0 && vanus > 100)
+            {
+                hinn = "viga andmetega";
+            }
+            else
+            {
+                hinn = "???";
+            }
+            return hinn;
+        }
+        public static string Pinginaaber(string nimi)
+        {
+
+            string vastus = "";
+            if (nimi == "Mark" || nimi == "Marek")
+            {
+                vastus = "Te olete pinginaabrid";
+            }
+            else
+            {
+                vastus = "Tee ei olete pinginaabrid";
+            }
+            return vastus;
+        }
+        public static int Remont(int pikkus, int laius)
+        {
+            return pikkus * laius;
+        }
+
     }
-}
+    }
--- Startclass.cs
+++ Startclass.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
 namespace Kordamine_TARpv24
 {
@@ -10,32 +10,146 @@ namespace Kordamine_TARpv24
     {
         public static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.BackgroundColor = ConsoleColor.Red;
-            Console.ForegroundColor = ConsoleColor.Green;
-            
-            Console.WriteLine("Tere Tulemast!, Mis on sinu nimi?");
-            string tekst = Console.ReadLine();
-            Console.WriteLine($"{tekst}, Rõõm näha!");
-            int a = 10;
-            char taht = 'A';
-            Console.Write($"Esimene arv on {a}, sisesta b = ....");
-            int b = int.Parse(Console.ReadLine());
-            Console.WriteLine($"Vastus on = {a + b}");
-            Console.WriteLine("Ujukomaarv");
-            double d = double.Parse(Console.ReadLine());
-            Console.WriteLine(d);
-            float f = float.Parse(Console.ReadLine());
-            Console.WriteLine(f);
-            bool g = false;
+            Console.WriteLine("Mis on sinu nimi");
+            string nimi = Console.ReadLine();
+            if (nimi.ToLower() == "juku")
+            {
+                Console.WriteLine("Kui vana sa oled");
+                int vanus = int.Parse(Console.ReadLine());
+                string pilet = Osa1_funktsioonid.Kino(vanus);
+                string hinn = Osa1_funktsioonid.Kino(vanus);
+                Console.WriteLine($"Sinu pilet on {hinn}");
+            }
+            else
+            {
+                Console.WriteLine("Mina pean Jukuga kino minna");
+            }
 
-            Random rnd = new Random();
-            a = rnd.Next(-100, 200);
-            Console.WriteLine(a);
 
-            float vastus = Osa1_funktsioonid.Kalkulaator(f, a);
-            Console.WriteLine($"Kalkulaatori tulemus: {vastus}");
 
+
+            Console.WriteLine("Mis on sinu nimi");
+            string nimi1 = Console.ReadLine();
+            Console.WriteLine("Aga mis on sinu nimi?");
+            string nimi2 = Console.ReadLine();
+            if (nimi1 == "Mark" || nimi2 == "Marek")
+            {
+                Console.WriteLine("Te olete pinginaabrid");
+            }
+            else
+            {
+                Console.WriteLine("te ei olete pinginaabrid");
+            }
+
+
+
+            Console.Write("Sisesta ruumi pikkus: ");
+            int pikkus = int.Parse(Console.ReadLine());
+
+            Console.Write("Sisesta ruumi laius: ");
+            int laius = int.Parse(Console.ReadLine());
+
+            int pindala = Osa1_funktsioonid.Remont(pikkus, laius);
+
+            Console.WriteLine($"Põranda pindala on {pindala} ruutmeetrit.");
+
+
+            //Console.WriteLine("Kui vana sa oled");
+            //int vanus = int.Parse(Console.ReadLine());
+            //string pilet = Osa1_funktsioonid.Kino(vanus);
+            //string hinn = Osa1_funktsioonid.Kino(vanus);
+            //Console.WriteLine($"Sinu pilet on {hinn}");
+
+
+
+
+            //Random rnd = new Random();
+            //string Kinoo = Osa1_funktsioonid.Kino(vanus);
+            //Console.WriteLine("Kirjuta sinu vanus");
+            //int.Parse(Console.ReadLine());
+
+
+
+
+
+
+
+
+
+
+
+            //2. Osa valikud
+            //int kuu_number = rnd.Next(1, 12);
+            //string nimetus = Osa1_funktsioonid.Kuu_nimetus(kuu_number);
+            //Console.WriteLine($"Nr: {kuu_number} - {nimetus}");
+            //Console.WriteLine("Kas tahad dekodeerida arv->nimetusse?");
+            //string vastus = Console.ReadLine();
+            //if (vastus.ToLower() != "jah")
+            //{
+            //    Console.WriteLine("Ei taha");
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        Console.Write("Nr: ");
+            //        kuu_number = int.Parse(Console.ReadLine());
+            //        Console.WriteLine(Osa1_funktsioonid.Hooaeg(kuu_number));
+            //    }
+            //    catch (Exception e)
+            //    {
+            //        Console.WriteLine(e);
+            //    }
+            //}
         }
     }
 }
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//            Console.OutputEncoding = Encoding.UTF8;
+//            Console.BackgroundColor = ConsoleColor.Red;
+//            Console.ForegroundColor = ConsoleColor.Green;
+
+//            Console.WriteLine("Tere Tulemast!, Mis on sinu nimi?");
+//            string tekst = Console.ReadLine();
+//            Console.WriteLine($"{tekst}, Rõõm näha!");
+//            int a = 10;
+//            char taht = 'A'; //üks täht
+//            Console.Write($"Esimene arv on {a}, sisesta b = ....");
+//            int b = int.Parse(Console.ReadLine());
+//            Console.WriteLine($"Vastus on = {a + b}");
+//            Console.WriteLine("Ujukomaarv");
+//            double d = double.Parse(Console.ReadLine());
+//            Console.WriteLine(d);
+//            float f = float.Parse(Console.ReadLine());
+//            Console.WriteLine(f);
+//            bool g = false;
+
+//            a = rnd.Next(-100, 200);
+//            Console.WriteLine(a);
+
+//            float vastus = Osa1_funktsioonid.Kalkulaator(f, a);
+//            Console.WriteLine($"Kalkulaatori tulemus: {vastus}");
+
+//        }
+//    }
+//}
+
+
+
+//2. osa valikud
+
+
